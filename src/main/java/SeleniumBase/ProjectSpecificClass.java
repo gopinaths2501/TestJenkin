10d0f75b660e4ab8302b83f8368ec475efe3cd65
Gopi <Gopi@DESKTOP-I533CSV>
2020-05-27 13:25:18 +0530

    TestJenkin

--- src/main/java/SeleniumBase/ProjectSpecificClass.java
+++ src/main/java/SeleniumBase/ProjectSpecificClass.java
@@ -1,44 +1,29 @@
 package SeleniumBase;
 
-import java.util.concurrent.TimeUnit;
-
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 
-public class ProjectSpecificClass {
-	
-	public static ChromeDriver driver;	
-    public String excelFileName;
-    
-	
-	@BeforeMethod
-
-	public void RunCreateEdit1 () {
-		
-		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
-		driver = new ChromeDriver();
-		driver.manage().window().maximize();
-		driver.get("http://leaftaps.com/opentaps/");
-		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-		
-		System.out.println("I am @BeforeMethod");
+		public class ProjectSpecificClass {
+			
+			public static void main (String [] args) throws InterruptedException
+			{
+				System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
+				
+				WebDriver driver= new ChromeDriver();
+				
+				driver.manage().window().maximize();
+				
+				driver.get("https://www.facebook.com");
 	}
 		
-        @AfterMethod
-		
-		public void RunCreateEdit2 () {	
-		
-			driver.close();
+        
 			
-			System.out.println("I am @AfterMethod");
-			
+		
+		
 	
 	}
         
         	
             
-}
-        
         
 
